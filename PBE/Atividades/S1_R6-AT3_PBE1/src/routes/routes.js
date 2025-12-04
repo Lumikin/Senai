1b86d6888a69fcfa70da13d7c285b4c0ad1a0bee
Lumikin <lumikomio02@gmail.com>
2025-12-04 15:35:57 -0300

    hai

--- PBE/Atividades/S1_R6-AT3_PBE1/src/routes/routes.js
+++ PBE/Atividades/S1_R6-AT3_PBE1/src/routes/routes.js
@@ -2,12 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 //Referência do arquivo de rotas
-const { clienteRoutes } = require('./clienteRoutes')
 const { pedidoRoutes } = require('./pedidoRoutes')
 const { entregaRoutes } = require('./entregaRoutes')
 
 router.use('/', pedidoRoutes);
-router.use('/', clienteRoutes);
 router.use('/', entregaRoutes);
 
 module.exports = { router };
